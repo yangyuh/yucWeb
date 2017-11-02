036630464ffe11aba04f0ba4ece8dfa43ae86ba3
yangyh <136585913@qq.com>
2017-11-02 23:35:42 +0800

    直播回放和个人中心

---
+++ yucaijy/public/libs/js/liveAllOrder.js
@@ -0,0 +1,24 @@
+$(function(){
+	$(".orderlist-nav li").on("click", function() {
+		$(".orderlist-nav").find("li").removeClass("active");
+		$(this).addClass("active");
+	})
+	getMyClassTpl()
+})
+
+// 加载我的订单页面
+function getMyOrderTpl(){
+	var view = $('#contentRightView');
+	var tpl = $('#myOrderTpl').html();
+	laytpl(tpl).render([], function(html) {
+		view.html(html);
+	});
+}
+// 加载我的课程页面
+function getMyClassTpl(){
+	var view = $('#contentRightView');
+	var tpl = $('#myClassTpl').html();
+	laytpl(tpl).render([], function(html) {
+		view.html(html);
+	});
+}
